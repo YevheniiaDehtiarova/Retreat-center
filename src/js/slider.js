3884d1d673534222696970846633a732f0e0018b
Yevheniia Dehtiarova <Yevheniia.Dehtiarova@redsailtechnologies.com>
2024-08-31 23:26:19 +0300

    create 2 sections

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,44 +1,20 @@
 
 
-$('.news__slider').slick({
-    prevArrow: '<img src="src/assets/img/arrow-left.svg"  class="prev" alt="Left Arrow">',
-    nextArrow: '<img src="src/assets/img/arrow-right.svg" class="next" alt="Right Arrow">',
+$('.anticipation__slider').slick({
+    prevArrow: '<img src="src/assets/img/slider-arrow-left.png"  class="prev" alt="Left Arrow">',
+    nextArrow: '<img src="src/assets/img/slider-arrow-right.png" class="next" alt="Right Arrow">',
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: true,
-    centerPadding: '10px',
-    responsive: [{
-        breakpoint: 821,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: false,
-        }
-    }]
+    centerPadding: '0px',
 });
 
 
-$('.volunteering__slider').slick({
-    prevArrow: '<img src="src/assets/img/arrow-left.svg"  class="prev" alt="Left Arrow">',
-    nextArrow: '<img src="src/assets/img/arrow-right.svg" class="next" alt="Right Arrow">',
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [{
-        breakpoint: 821,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: false
-        }
-    }]
-});
+
 
 
 $(window).on('resize', function() {
-    $('.news__slider').slick('setPosition');
+    $('.anticipation__slider').slick('setPosition');
 });
-$(window).on('resize', function() {
-    $('.volunteering__slider').slick('setPosition');
-});
+;
