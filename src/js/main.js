c137a4d7bb872864a07b3d0152719519c3121868
Yevheniia Dehtiarova <Yevheniia.Dehtiarova@redsailtechnologies.com>
2024-09-09 23:18:26 +0300

    add media for phone size

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 (function() {
+    let burgerButton = document.querySelector('.burger-button');
+    let burgerMenu = document.querySelector('.mobile-menu-wrapper');
+    let logo = document.querySelector('.header__logo')
+
+
+
+    burgerButton.addEventListener('click', burgerClass);
+
+    function burgerClass() {
+        burgerButton.classList.toggle('burger-button-active');
+        burgerMenu.classList.toggle('hidden');
+        logo.classList.toggle('hidden');
+    };
+
     const tabs = document.querySelectorAll('.tab');
     const slides = document.querySelectorAll('.anticipation__slider .slick-slide');
 
