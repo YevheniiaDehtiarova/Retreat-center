4f635e32b23fd6a1289699d1fe0f0c688881cf81
Yevheniia Dehtiarova <Yevheniia.Dehtiarova@redsailtechnologies.com>
2024-09-13 09:34:27 +0300

    delete console

--- src/js/main.js
+++ src/js/main.js
@@ -16,15 +16,9 @@
     const tabs = document.querySelectorAll('.tab');
     const slides = document.querySelectorAll('.anticipation__slider .slick-slide');
 
-    console.log(slides);
-
     let activeSlide = 0;
 
     function changeSlide(index) {
-  
-
-        console.log(index,slides[index])
-
         slides[index].classList.add('active');
         tabs[index].classList.add('active');
 
